c8b663b38a2ea732d322116a5baf2916d8d503a9
smrithwindev <rithwindev2002@gmail.com>
2025-08-09 17:18:11 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,7 +79,14 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {          // h
             );
         }
 
-        console.log(cart);
+       let cartQuantity=0;
+
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
 
 });
+
