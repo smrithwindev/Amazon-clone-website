3ade8dcb1904cedf6c75d128707445cfd4fff996
smrithwindev <rithwindev2002@gmail.com>
2025-08-14 11:11:23 +0530

    created separate function for formatCurrency

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartSummaryHTML='';
 
@@ -28,7 +29,7 @@ cart.forEach((cartItems) =>{
                   ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $${(matchingProduct.priceCents /100).toFixed(2)}
+                  $${formatCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
